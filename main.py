d0cf4cedacf620984b61d062f97105c80dbf6e6e
Bkstandukar <bkstandukar9@gmail.com>
2021-03-15 21:13:25 +0100

    added comment on functions

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import argparse
 from DependentFinder import DependentFinder
 import csv
+import time
 
 
 def read_csv(filename):
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
+        # convert nested loop to single array
         repo_list_from_csv = [item for sublist in reader for item in sublist]
     return repo_list_from_csv
 
 
 def convert_repo_to_url(repo_list):
+    # repo argument is converted to github url
     new_url_list = [f"https://github.com/{x}/network/dependents" for x in repo_list]
     return new_url_list
 
@@ -19,9 +22,10 @@ if __name__ == "__main__":
     argp = argparse.ArgumentParser(description="Find direct dependents using Web Scraping")
     argp.add_argument('--filename', type=str, default="repos.csv", help='File name eg: repos.csv')
     args = argp.parse_args()
-    print("workng on getting Depencency")
+    print("working on getting Dependents")
     repos = read_csv(args.filename)
     url_list = convert_repo_to_url(repos)
+    time.sleep(2)
     DependentFinder().multiple_run(url_list)
 
 
