82ec1af399b02823ec1a5d8d9049e4e171d7affe
Bkstandukar <bkstandukar9@gmail.com>
2021-03-31 23:04:09 +0200

    added API support for repository information

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import argparse
+from API_Dependents import get_token_key, get_test_data
 from DependentFinder import DependentFinder
 import csv
 import time
+import pandas as pd
+import os
+
+time_str_file = time.strftime("%m-%d-%Y_%H-%M-%S")
 
 
 def read_csv(filename):
@@ -18,6 +23,13 @@ def convert_repo_to_url(repo_list):
     return new_url_list
 
 
+def get_file_name(file_name, directory_name=None):
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
+    filename = f"{directory_name}\{file_name}_{time_str_file}.csv"
+    return filename
+
+
 if __name__ == "__main__":
     argp = argparse.ArgumentParser(description="Find direct dependents using Web Scraping")
     argp.add_argument('--filename', type=str, default="repos.csv", help='File name eg: repos.csv')
@@ -26,6 +38,25 @@ if __name__ == "__main__":
     repos = read_csv(args.filename)
     url_list = convert_repo_to_url(repos)
     time.sleep(2)
-    DependentFinder().multiple_run(url_list)
+    get_dependent = DependentFinder().multiple_run(url_list)
+    # making list of dependents ready for API part
+    uniq_df = get_dependent.drop_duplicates()
+    print("pulling repo Information dor dependents...")
 
+    dependent_list = [x for x in uniq_df["Dependent"] if x != "No Dependent"]
 
+    data_table = get_test_data(get_token_key(), dependent_list)
+    repo_dependencies_df = pd.json_normalize(data_table, max_level=500)
+    file_to_save = get_file_name("all_dependents", directory_name="dependent_results")
+    try:
+        combined_df = repo_dependencies_df.merge(uniq_df, how="right", on="Dependent")
+    except KeyError:
+        print("There is not Dependent to work")
+        uniq_df.to_csv(file_to_save, index=False)
+    else:
+        col_name = "Repos"
+        first_col = combined_df.pop(col_name)
+        combined_df.insert(0, col_name, first_col)
+        combined_df.to_csv(file_to_save, index=False)
+
+    print(f"file is saved as: {file_to_save}")
